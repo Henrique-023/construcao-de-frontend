74d514b8ab94d23bf26d0f8bfad7502a6be92eba
unknown <2314290017@ACADEMICO.IESB.BR>
2024-09-12 17:54:10 -0300

    Adicionando a Pratica04 sobre React

---
+++ praticas/pratica04/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './componentes/Header';
+
+function App(){
+  return(
+    <div className='card'>
+      <Header />
+      <p>Este é meu parágrafo em JSX!</p>
+    </div>
+    );
+
+
+}
+
+export default App;
---
+++ praticas/pratica04/src/componentes/Header.jsx
@@ -0,0 +1,9 @@
+import './Header.css';
+
+function Header(){
+    return(
+        <header> <h1>Minha aplição React</h1></header>
+    );
+}
+
+export default Header;
